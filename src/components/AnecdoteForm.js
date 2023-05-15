c43f89aab5c3b8869acc06a6dea9c109baa82a83
johnny <jhuang007007@gmail.com>
2023-05-15 14:48:08 -0400

    completed exercise 6.20-6.22

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,10 +1,9 @@
-const AnecdoteForm = () => {
-
+const AnecdoteForm = ({anecdoteMutation}) => {
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    anecdoteMutation.mutate({ content, votes: 0 })
 }
 
   return (
